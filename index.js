a2ab5ebc9d0d884b5a5ede779e16022ac668d02a
LemonHaque <azizulhaquelemon31@gmail.com>
2023-05-20 14:45:10 +0600

    added post method

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+
         const toyCollection = client.db("toyVerse").collection('Car');
 
+
+       
         app.get('/car', async (req, res) => {
             const cursor = await toyCollection.find()
             const result = await cursor.toArray()
@@ -43,6 +46,19 @@ async function run() {
                 return res.send(result)
             }
             const result = await toyCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        app.get('/toy', async(req,res)=>{
+            const cursor =toyCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/toy', async (req, res) => {
+            const newToy = req.body;
+            console.log(newToy);
+            const result = await toyCollection.insertOne(newToy)
             res.send(result)
         })
 
